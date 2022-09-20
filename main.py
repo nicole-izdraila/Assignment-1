c605a35bb7c065a95209b1c50d229f768638bf1e
Axeloooo <axelshz@gmail.com>
2022-09-20 09:30:16 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Functions
+
+def myAdd(x, y):
+    return x + y
+
+def mySub(x, y):
+    return x - y
+
+def myMul(x, y):
+    return x * y
+
+def myDiv(x, y):
+    return x // y
+
+def main():
+    
+    return
+
+if __name__ == "__main__":
+    main()
